5ecca4f8c4e837a4532f33a0a1210b180cf99009
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-20 20:56:08 +0200

    7-2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,4 @@
 
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
@@ -32,21 +23,25 @@ const galleryImage = galleryItems.map(({ preview, original, description })=>{
 const stringGallery = galleryImage.join('');
 divGallery.innerHTML = stringGallery;
 
-console.log(galleryItems);
-console.log(galleryImage);
-console.log(stringGallery);
-
 divGallery.addEventListener('click', onDivGalleryClick);
 
 function onDivGalleryClick(event) {
-    event.preventDefolt();
+    event.preventDefault();
     if (!event.target.classList.contains('gallery__image')) {
         return;
     };
 
-    // const instance = basicLightbox.create(
-    //     <img src="${event.target.dataset.source}" width="800" height="600"/>
-    // );
-}
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600"/>
+`);
+instance.show();
 
-
+divGallery.addEventListener('keydown', onEscPressModalClose);
+    
+    function onEscPressModalClose(event) {
+        if (event.code === "Escape") {
+        instance.close();
+        divGallery.removeEventListener('keydown', onEscPressModalClose);
+        }
+    };
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+
+const galleryImage = galleryItems.map(({ preview, original, description })=>{
+    const photoEl = `
+    <a class="gallery__item"
+        href="${original}">
+        <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"
+        />
+    </a>
+    `;
+    return photoEl;
+});
+
+const stringGallery = galleryImage.join('');
+divGallery.innerHTML = stringGallery;
+
+divGallery.addEventListener('click', onDivGalleryClick);
+
+function onDivGalleryClick(event) {
+    event.preventDefault();
+    var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt"});
+};
